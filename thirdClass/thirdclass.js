2a9f1fadcc415c505e4077822b3ef4fc458a975b
Jahid Ahmed <ajahid97@gmail.com>
2025-02-18 08:38:03 +0600

    Updated

--- thirdClass/thirdclass.js
+++ thirdClass/thirdclass.js
@@ -1,3 +1,43 @@
+//for loops
 for(let i=1; i<=5; i++){
     console.log("Jahid Ahmed!!")
 }
+
+
+//while loop
+let i = 1;
+while(i<=5){
+    console.log("i is: ", i);
+    i++;
+}
+
+
+//do while loop
+let x = 1;
+do{
+    console.log("x is: ", x);
+    x++
+}while(x<=10);
+
+//for of loop
+//note this loop use for String and Arrays
+let str = "JAHID AHMED";
+for(let ver of str){
+    console.log("Character is: ", ver);
+}
+
+
+//for in loop
+//note this loop use for object
+for(let i=1; i<=100; i+=2){
+    console.log("Even number is: ", i);
+}
+
+
+let gameNumber = 53470;
+let guessNumber = prompt("Guess the number: ");
+
+while(gameNumber !=guessNumber){
+    guessNumber = prompt("Yor guess wrong number! Choose again!!");
+}
+console.log("Congratulations! You guess the right one!")
