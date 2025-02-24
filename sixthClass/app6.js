26728bc67aa2fd3e33fd26b3babaf80d07078ab9
Jahid Ahmed <ajahid97@gmail.com>
2025-02-24 07:15:29 +0600

    up

---
+++ sixthClass/app6.js
@@ -0,0 +1,3 @@
+console.log(document.body.childNodes[1]);
+document.getElementById('heading').innerText = 'Learn JavaScript!';
+document.getElementById('paragraph').innerHTML = '<i>We are learning JavaScript DOM menupulation!.</i>';
