c6a0ea01e5c78811c2277255553ee42083594589
Jahid Ahmed <ajahid97@gmail.com>
2025-02-26 06:55:08 +0600

    c 7

---
+++ seventhClass/app7.js
@@ -0,0 +1,7 @@
+let head = document.getElementById('myID');
+let id = head.getAttribute('id');
+console.log(id);
+
+let para = document.querySelector('.myClassAtt');
+para.setAttribute("class", "myClass");
+console.log(para);
